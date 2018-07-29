ef9580d040ffc7767133600024efcb9b6986f3b9
Kurt Peek <kurtpeek@Kurts-MacBook-Pro.local>
2018-07-29 16:23:39 -0700

    Added chevrons to the Publications section

--- js/accordion-chevrons.js
+++ js/accordion-chevrons.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-    $("#accordion button").click(function(){
-        $("#accordion button .mdi").removeClass("mdi-chevron-up");
-        $("#accordion button .mdi").removeClass("mdi-chevron-down");
-    });
+  function toggleChevron(e) {
+    $(e.target)
+      .prev('.card-header')
+      .find("i.mdi")
+      .toggleClass('mdi-chevron-down mdi-chevron-up');
+  }
+
+  $('#accordion-publications').on('hide.bs.collapse', toggleChevron);
+  $('#accordion-publications').on('show.bs.collapse', toggleChevron);
 });
