315d197db23f6a0e68ea6afe48b9ebcc371a3227
Kurt Peek <kurtpeek@Kurts-MacBook-Pro.local>
2018-07-29 16:40:25 -0700

    Added chevrons to accordion in Services section

--- js/accordion-chevrons.js
+++ js/accordion-chevrons.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // Add up/down chevron icons to accordion, similar to https://www.w3.org/TR/wai-aria-practices-1.1/examples/accordion/accordion.html
   function toggleChevron(e) {
     $(e.target)
       .prev('.card-header')
@@ -6,6 +7,11 @@ $(document).ready(function(){
       .toggleClass('mdi-chevron-down mdi-chevron-up');
   }
 
+  // Services section
+  $('#accordion').on('hide.bs.collapse', toggleChevron);
+  $('#accordion').on('show.bs.collapse', toggleChevron);
+
+  // Publications section
   $('#accordion-publications').on('hide.bs.collapse', toggleChevron);
   $('#accordion-publications').on('show.bs.collapse', toggleChevron);
 });
